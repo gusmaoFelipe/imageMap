6c31cb8d7dac105cb227b5e5567ebec39452c7b3
JeanHFerreira <JeanHFerreira@github.com>
2015-11-25 10:57:22 -0200

    Alteração local de classes de teste

---
+++ ImageMapEditor/src/Testes/CircleShapeTest.java
@@ -0,0 +1,39 @@
+package Testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CircleShapeTest {
+
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		
+	}
+	
+	@Test
+	public void test3() {
+		
+	}
+	
+	@Test
+	public void test4() {
+		
+	}
+	
+	@Test
+	public void test5() {
+		
+	}
+	
+	@Test
+	public void test6() {
+		
+	}
+
+}
